a5216bdbeaba7301af89bfe7ded8d838bd0d7aa7
Sindre Sorhus <sindresorhus@gmail.com>
2014-02-28 20:54:27 +0100

    init

---
+++ test.js
@@ -0,0 +1,21 @@
+'use strict';
+var assert = require('assert');
+var fs = require('fs');
+var gzipSize = require('./index');
+var a = fs.readFileSync('test.js', 'utf8');
+
+describe('gzipSize()', function () {
+	it('should get the gzipped size', function (cb) {
+		gzipSize(a, function (err, size) {
+			assert(!err);
+			assert(size < a.length);
+			cb();
+		});
+	});
+});
+
+describe('gzipSize.sync()', function () {
+	it('should get the gzipped size', function () {
+		assert(gzipSize.sync(a) < a.length);
+	});
+});
