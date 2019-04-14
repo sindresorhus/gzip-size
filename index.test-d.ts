2bdfefc2b5a66252d00f8275382c5a420021959a
Dimitri Benin <dimitri.benin@gmail.com>
2019-04-15 00:28:15 +0200

    Add TypeScript definition

---
+++ index.test-d.ts
@@ -0,0 +1,16 @@
+import {expectType} from 'tsd';
+import * as fs from 'fs';
+import gzipSize = require('.');
+
+expectType<Promise<number>>(gzipSize('foo'));
+expectType<Promise<number>>(gzipSize('foo', {chunkSize: 1}));
+expectType<number>(gzipSize.sync('foo'));
+expectType<number>(gzipSize.sync('foo', {chunkSize: 1}));
+const gstream = fs
+	.createReadStream('index.d.ts')
+	.pipe(gzipSize.stream())
+	.pipe(gzipSize.stream({chunkSize: 1}))
+	.on('gzip-size', size => expectType<number>(size));
+expectType<number | undefined>(gstream.gzipSize);
+expectType<Promise<number>>(gzipSize.file('index.d.ts'));
+expectType<Promise<number>>(gzipSize.file('index.d.ts', {chunkSize: 1}));
--- test.js
+++ test.js
@@ -1,47 +1,47 @@
 import fs from 'fs';
 import test from 'ava';
 import pEvent from 'p-event';
-import m from '.';
+import gzipSize from '.';
 
 const fixture = fs.readFileSync('test.js', 'utf8');
 
 test('get the gzipped size', async t => {
-	t.true(await m(fixture) < fixture.length);
+	t.true(await gzipSize(fixture) < fixture.length);
 });
 
 test('gzip compression level', async t => {
-	t.true(await m(fixture, {level: 6}) < await m(fixture, {level: 1}));
+	t.true(await gzipSize(fixture, {level: 6}) < await gzipSize(fixture, {level: 1}));
 });
 
 test('sync - get the gzipped size', t => {
-	t.true(m.sync(fixture) < fixture.length);
+	t.true(gzipSize.sync(fixture) < fixture.length);
 });
 
 test('sync - match async version', async t => {
-	t.is(m.sync(fixture), await m(fixture));
+	t.is(gzipSize.sync(fixture), await gzipSize(fixture));
 });
 
 test('sync - gzip compression level', t => {
-	t.true(m.sync(fixture, {level: 6}) < m.sync(fixture, {level: 1}));
+	t.true(gzipSize.sync(fixture, {level: 6}) < gzipSize.sync(fixture, {level: 1}));
 });
 
 test('stream', async t => {
-	const stream = fs.createReadStream('test.js').pipe(m.stream());
+	const stream = fs.createReadStream('test.js').pipe(gzipSize.stream());
 	await pEvent(stream, 'end');
-	t.is(stream.gzipSize, m.sync(fixture));
+	t.is(stream.gzipSize, gzipSize.sync(fixture));
 });
 
 test('gzip-size event', async t => {
-	const stream = fs.createReadStream('test.js').pipe(m.stream());
+	const stream = fs.createReadStream('test.js').pipe(gzipSize.stream());
 	const size = await pEvent(stream, 'gzip-size');
-	t.is(size, m.sync(fixture));
+	t.is(size, gzipSize.sync(fixture));
 });
 
 test('passthrough', async t => {
 	let out = '';
 
 	const stream = fs.createReadStream('test.js')
-		.pipe(m.stream())
+		.pipe(gzipSize.stream())
 		.on('data', buffer => {
 			out += buffer;
 		});
@@ -51,13 +51,13 @@ test('passthrough', async t => {
 });
 
 test('file - get the gzipped size', async t => {
-	t.true(await m.file('test.js') < fixture.length);
+	t.true(await gzipSize.file('test.js') < fixture.length);
 });
 
 test('fileSync - get the gzipped size', t => {
-	t.is(m.fileSync('test.js'), m.sync(fixture));
+	t.is(gzipSize.fileSync('test.js'), gzipSize.sync(fixture));
 });
 
 test('file - match async version', async t => {
-	t.is(await m.file('test.js'), await m(fixture));
+	t.is(await gzipSize.file('test.js'), await gzipSize(fixture));
 });
